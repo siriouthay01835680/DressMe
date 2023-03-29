36de76a91752925f03604cd756376401d9d97ee0
siriouthay01835680 <54681916+siriouthay01835680@users.noreply.github.com>
2023-03-29 13:24:50 -0400

    adding honor statement

--- app/src/main/java/com/mobileapp/dressme/MainActivity.java
+++ app/src/main/java/com/mobileapp/dressme/MainActivity.java
@@ -1,3 +1,12 @@
+/*
+Mobile App Development II -- COMP.4631 Honor Statement
+The practice of good ethical behavior is essential for maintaining good order in the classroom, providing an enriching learning experience for students, and training as a practicing computing professional upon graduation. This practice is manifested in the University's Academic Integrity policy. Students are expected to strictly avoid academic dishonesty and adhere to the Academic Integrity policy as outlined in the course catalog. Violations will be dealt with as outlined therein. All programming assignments in this class are to be done by the student alone unless otherwise specified. No outside help is permitted except the instructor and approved tutors.
+
+I certify that the work submitted with this assignment is mine and was generated in a manner consistent with this document, the course academic policy on the course website on Blackboard, and the UMass Lowell academic code.
+
+Date: 3/29/2023
+Name: Aria Siriouthay & Krestina Beshara
+*/
 package com.mobileapp.dressme;
 
 import android.content.Intent;
