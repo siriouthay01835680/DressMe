39f488b05fd124d5219963ad5a84e5c921e6d43f
KrestinaBeshara <98134097+KrestinaBeshara01842423@users.noreply.github.com>
2023-03-29 12:52:05 -0400

    added camera API
    Need to adjust screen to take pictures and fix where the upload button is

--- app/src/main/java/com/mobileapp/dressme/HomeScreen.java
+++ app/src/main/java/com/mobileapp/dressme/HomeScreen.java
@@ -1,18 +1,15 @@
 package com.mobileapp.dressme;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 public class HomeScreen extends Fragment {
 
@@ -35,13 +32,14 @@ public class HomeScreen extends Fragment {
                 Navigation.findNavController(view).navigate(R.id.action_homeScreen_to_dressMe2);
             }
         });
+        /*
         Button uploadBtn = view.findViewById(R.id.uploadButton);
         uploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(view).navigate(R.id.action_homeScreen_to_upload2);
             }
-        });
+        }); */
 
         return view;
     }
