68316d2c6f4a24497235ab8ba14fc1a0c4aa0bc9
siriouthay01835680 <54681916+siriouthay01835680@users.noreply.github.com>
2023-04-07 18:32:53 -0400

    Completed functionality of Outfit Gen.
    You can now go to dressme, generate an outfit (testing implementation, final will be done once images are sorted out), and send it to scrapbook which will appear in the first grid for now.

--- app/src/main/java/com/mobileapp/dressme/DressMe.java
+++ app/src/main/java/com/mobileapp/dressme/DressMe.java
@@ -1,18 +1,24 @@
 package com.mobileapp.dressme;
 
+//import static android.support.v4.media.session.MediaControllerCompatApi21.getPackageName;
 import static android.view.View.VISIBLE;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class DressMe extends Fragment {
 
@@ -28,24 +34,81 @@ public class DressMe extends Fragment {
         View view = inflater.inflate(R.layout.fragment_dress_me,
                 container, false);
         Button generate = view.findViewById(R.id.generateBtn);
-        ImageView shirt = view.findViewById(R.id.genShirt);
-        ImageView pants = view.findViewById(R.id.genPant);
+//        ImageView shirt = view.findViewById(R.id.genShirt);
+//        ImageView pants = view.findViewById(R.id.genPant);
+
+        LinearLayout layout = view.findViewById(R.id.imgLayout);
+        Button regenerate = view.findViewById(R.id.regenerateBtn);
+        final String[][] resultItems = new String[1][1];
         generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                shirt.setVisibility(view.VISIBLE);
-                pants.setVisibility(view.VISIBLE);
+//                shirt.setVisibility(view.VISIBLE);
+//                pants.setVisibility(view.VISIBLE);
+                //read from file to pick random shirt/pant
+                //outline for randomizer
+                //for now have array of test img ids
+                resultItems[0] = outfitGeneration(layout);
+//                System.out.println(resultItems[0][1]);
+
+            }
+        });
+        regenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                shirt.setVisibility(view.VISIBLE);
+//                pants.setVisibility(view.VISIBLE);
+                //read from file to pick random shirt/pant
+                //outline for randomizer
+                //for now have array of test img ids
+                layout.removeAllViewsInLayout();
+                resultItems[0] = outfitGeneration(layout);
+
             }
         });
         Button scrapbookBtn = view.findViewById(R.id.scrapbookBtn);
         scrapbookBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_dressMe_to_scrapbook);
+                DressMeDirections.ActionDressMeToScrapbook action = DressMeDirections.actionDressMeToScrapbook(resultItems[0][0], resultItems[0][1]);
+                Navigation.findNavController(view).navigate(action);
             }
         });
 
         return view;
     }
+    public String[] outfitGeneration(LinearLayout layout){
+        String[] shirtIds = {"orangeshirt", "testshirt"};
+        String[] pantsIds = {"testpants"};
+        Random rand = new Random();
+        String randShirt = shirtIds[1];
+        String randPants = pantsIds[0];
+//                for(int i = 0; i < itemIDs.length; i++){
+//
+//                }
+        //dynamically create new imageview w/ clothing image
+        ImageView img = new ImageView(layout.getContext());
+        int id = 100;
+        img.setId(id);
+        img.setLayoutParams(new android.view.ViewGroup.LayoutParams(500,500));
+//                img.setImageDrawable(getResources().getDrawable(R.drawable.testshirt));
+        int resId = getResources().getIdentifier(randShirt, "drawable", "com.mobileapp.dressme");
+//                System.out.println("res" + resId);
+//                System.out.println(shirtIds[0]);
+//                img.setImageDrawable(getResources().getDrawable(getResources().getIdentifier(randShirt, "drawable", "com.mobileapp.dressme")));
 
+        img.setImageDrawable(getResources().getDrawable(resId));
+        layout.addView(img);
+
+        ImageView img1 = new ImageView(layout.getContext());
+        id = 101;
+        img1.setId(id);
+        img1.setLayoutParams(new android.view.ViewGroup.LayoutParams(500,500));
+//                img1.setImageDrawable(getResources().getDrawable(R.drawable.testpants));
+        resId = getResources().getIdentifier(randPants, "drawable", "com.mobileapp.dressme");
+        img1.setImageDrawable(getResources().getDrawable(resId));
+        layout.addView(img1);
+        String[] result = {randShirt, randPants};
+        return result;
+    }
 }
--- app/src/main/java/com/mobileapp/dressme/Scrapbook.java
+++ app/src/main/java/com/mobileapp/dressme/Scrapbook.java
@@ -1,13 +1,20 @@
 package com.mobileapp.dressme;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 public class Scrapbook extends Fragment {
 
@@ -20,14 +27,35 @@ public class Scrapbook extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scrapbook, container, false);
+        View view = inflater.inflate(R.layout.fragment_scrapbook, container, false);
+
+        String shirt = ScrapbookArgs.fromBundle(getArguments()).getShirt();
+        String pants = ScrapbookArgs.fromBundle(getArguments()).getPants();
+        RelativeLayout layout = view.findViewById(R.id.relativeLayout1);
+
+        ImageView img = new ImageView(layout.getContext());
+        int id = 110;
+        img.setId(id);
+        img.setLayoutParams(new android.view.ViewGroup.LayoutParams(250,250));
+        int resId = getResources().getIdentifier(shirt, "drawable", "com.mobileapp.dressme");
+
+        img.setImageDrawable(getResources().getDrawable(resId));
+        layout.addView(img);
+
+        ImageView img1 = new ImageView(layout.getContext());
+        id = 111;
+        img1.setId(id);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(250, 250);
+        img1.setLayoutParams(lp);
+
+        resId = getResources().getIdentifier(pants, "drawable", "com.mobileapp.dressme");
+
+        img1.setImageDrawable(getResources().getDrawable(resId));
+        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        layout.addView(img1);
+
+        return view;
     }
-/*
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ScrapbookViewModel.class);
-        // TODO: Use the ViewModel
-    } */
+
 
 }
