e6bcb009aa6eae56f7533998a9e63ed07c210bf1
KrestinaBeshara <98134097+KrestinaBeshara01842423@users.noreply.github.com>
2023-04-29 20:17:33 -0400

    comments

--- app/src/main/java/com/mobileapp/dressme/HomeScreen.java
+++ app/src/main/java/com/mobileapp/dressme/HomeScreen.java
@@ -25,23 +25,28 @@ public class HomeScreen extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_screen,
                 container, false);
-
+        //creating the animation for the dress me button
         Animation pulseAnimation = new ScaleAnimation(
                 1.0f, 1.1f, 1.0f, 1.1f,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f
+                //setting up the animation to pulse with these defined scales
         );
         pulseAnimation.setDuration(1000);
+        //setting the duration to 1000
         pulseAnimation.setRepeatMode(Animation.REVERSE);
         pulseAnimation.setRepeatCount(Animation.INFINITE);
+        //setting the animation to repeat infinitely as long as the app is running
 
         Button dressBtn = view.findViewById(R.id.dressMeButton);
         dressBtn.startAnimation(pulseAnimation);
+        //getting access to the dressme buttin and beginning the animation process
         System.out.println("here");
         dressBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(view).navigate(R.id.action_homeScreen_to_dressMe2);
+                //adding navigation to the dress me button to go to the dress me fragment
             }
         });
         /*
